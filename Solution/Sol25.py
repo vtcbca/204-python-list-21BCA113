cc25915569c3ecc786347a8dc577e73bacab4578
PRAJAPATI ANKIT KANAIYALAL <100261566+21BCA113@users.noreply.github.com>
2022-04-10 14:25:09 +0530

    Solution 25

---
+++ Solution/Sol25.py
@@ -0,0 +1,20 @@
+def unique(l):
+    a = []
+    for i in l:
+        if i not in a:
+            a.append(i)
+    return a
+
+
+def duplicate(a):
+    l = []
+    for i in range(a):
+        l.append(int(input('Enter any integer {} : '.format(i+1))))
+    a = unique(l)
+    return a
+
+
+if __name__ == '__main__':
+    a = int(input("Enter List Size : "))
+    a = duplicate(a)
+    print('Unique Values Of Your List : ', a)
